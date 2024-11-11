b6c6b77c1ffb76e6bae021181120f53c860025a6
wissam-bouznada <wissam.bouznada@etu.usthb.dz>
2024-11-11 18:08:36 +0100

    ajout des ficher

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+rigesterBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
